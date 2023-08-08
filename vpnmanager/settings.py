e1375cf3f1465b52618da76e730bca8e88171ea1
samir <samir.mousavian@gmail.com>
2023-08-08 10:18:08 +0330

    ALLOWED_HOSTS

--- vpnmanager/settings.py
+++ vpnmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a!5y4!11*om6766cj_-s)_wvj8=)e2nkgj)3$!r$c-lh)0buqg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['37.32.26.17', '127.0.0.1']
 
 
 # Application definition
